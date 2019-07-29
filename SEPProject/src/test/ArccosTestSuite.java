c4100afa936b42cefac1083a4dfcdce1b5ef924a
Himansi Patel <himansipatel1994@gmail.com>
2019-07-29 15:04:01 -0400

    #  Test suite created for Arccos

---
+++ SEPProject/src/test/ArccosTestSuite.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * This is an test suite case for Arccos Test Cases.
+ * 
+ * @author <a href="himansipatel1994@gmail.com">Himansi Patel</a>
+ * @version 0.0.1
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ArccosTest.class, HelperFunctionTest.class})
+public class ArccosTestSuite {
+
+}
